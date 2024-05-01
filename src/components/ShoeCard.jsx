ae4acad9208a7d8c4f7ab959e51e027e70ef7461
HawraaYounes <hawraayounes0@gmail.com>
2024-05-01 16:15:24 +0300

    finished hero section

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -7,7 +7,7 @@ const ShoeCard = ({ imgUrl, changeBigShoeImage, bigShoeImage }) => {
   return (
     <div
       className={`border-2 rounded-xl
-     ${bigShoeImage === imgUrl ? "border-coral-red" : "border-transparent"}
+     ${bigShoeImage === imgUrl.bigShoe ? "border-coral-red" : "border-transparent"}
      cursor-pointer max-sm:flex-1`}
       onClick={handleClick}
     >
